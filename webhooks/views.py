4cbe068cc3b5d69db15ed44fa0d2ce8f43416f07
Yi-An Chen <yi-anchen@yi-ande-mbp.home>
2019-08-27 00:30:14 -0700

    debug

--- webhooks/views.py
+++ webhooks/views.py
@@ -32,7 +32,7 @@ def callback(request):
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text_parser = TextParser(event.message.text)
-    print event.message.text
+    print (event.message.text)
     answer = text_parser.parse()
     line_bot_api.reply_message(
         event.reply_token,
