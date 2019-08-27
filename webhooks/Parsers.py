94da1d44026b13b359008cd118068d2e3df9335a
Yi-An Chen <yi-anchen@yi-ande-mbp.home>
2019-08-27 00:28:49 -0700

    debug

--- webhooks/Parsers.py
+++ webhooks/Parsers.py
@@ -45,7 +45,6 @@ class TextParser(Parser):
 
     def parse(self):
         question = self.message.split("\n+")
-        print question
         try:
             return self._answer(question[0], question[1])
 
