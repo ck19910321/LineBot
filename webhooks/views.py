4430ee0d7f47fbef767d0c2dc7317cee3a9ec36e
Yi-An Chen <yi-anchen@yi-ande-mbp.home>
2019-09-02 01:37:52 -0700

    where to put api?

--- webhooks/views.py
+++ webhooks/views.py
@@ -7,8 +7,14 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
 from webhooks.Parsers import TextParser
-from webhooks.line_api import handler
+# from webhooks.line_api import handler
 from webhooks.tasks import reply_delay
+
+from linebot import LineBotApi, WebhookHandler
+
+line_bot_api = LineBotApi('3fJbjOb+F4yeTpU1Kut6D7DfgZdjEwRabGqBkrTwT+5MFYBrFPWr7Tgs+jWcC9CdpgZmuYNmaUi/ML1X4ncwLq5pV1zD1UxAzkfhX1xdCt2rSpQYE+xR+aQyqWFIObFRR+/E8Yab/2b9IdN9GcNlogdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('d6dd24a1b7cc59462411284e955acd77')
+
 
 @csrf_exempt
 @require_POST
