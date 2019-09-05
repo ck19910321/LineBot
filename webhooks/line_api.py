f5cccbfa8ed2cd9c61a340306274a45284c8f9e2
Yi-An Chen <yi-anchen@yi-ande-mbp.home>
2019-09-05 01:16:46 -0700

    Revert "move handler to line api place"
    This reverts commit 98ff48a1b4b12e52c6a2420865f9173f66fe6564.

--- webhooks/line_api.py
+++ webhooks/line_api.py
@@ -1,53 +1,5 @@
-from urllib.parse import parse_qsl
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import MessageEvent, PostbackEvent, TextMessage, TextSendMessage
-
 from django.conf import settings
-
-from webhooks.Parsers import TextGenerator
-from webhooks.jobs import JOB_API
-
 
 line_bot_api = LineBotApi(settings.LINE_BOT_API)
 handler = WebhookHandler(settings.LINE_BOT_HANDLER)
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_text_message(event):
-    if event.source.type == "user":
-        text_generator = TextGenerator(event.message.text, user_id=event.source.user_id)
-    else:
-        text_generator = TextGenerator(
-            event.message.text,
-            user_id=event.source.user_id,
-            room_id=event.source.room_id,
-        )
-
-    message = text_generator.generate()
-    line_bot_api.reply_message(event.reply_token, message)
-
-
-@handler.add(PostbackEvent)
-def handle_post_text_message(event):
-    key = "{}_{}".format(event.source.user_id, getattr(event.source, "room_id", ""))
-    data, func_name, woody_type = _handle_postback_data(event.postback)
-    api = JOB_API[woody_type](data=data, key=key)
-    func = "can_{}".format(func_name)
-    try:
-        message = getattr(api, func)()
-
-    except AttributeError:
-        message = TextSendMessage(text="錯誤的訊息")
-
-    finally:
-        line_bot_api.reply_message(event.reply_token, message)
-
-
-def _handle_postback_data(postback):
-    data = {}
-    for pair in parse_qsl(postback.data):
-        data[pair[0]] = pair[1]
-
-    if getattr(postback, "params"):
-        data["target_datetime"] = postback.params["datetime"]
-    return data, data.pop("action"), data.pop("type")
--- webhooks/views.py
+++ webhooks/views.py
@@ -1,10 +1,15 @@
+from urllib.parse import parse_qsl
+
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, PostbackEvent, TextMessage, TextSendMessage
 
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
-from webhooks.line_api import handler
+from webhooks.Parsers import TextGenerator
+from webhooks.line_api import handler, line_bot_api
+from webhooks.jobs import JOB_API
 
 
 @csrf_exempt
@@ -22,3 +27,44 @@ def callback(request):
         return HttpResponseBadRequest()
 
     return HttpResponse("Ok")
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    if event.source.type == "user":
+        text_generator = TextGenerator(event.message.text, user_id=event.source.user_id)
+    else:
+        text_generator = TextGenerator(
+            event.message.text,
+            user_id=event.source.user_id,
+            room_id=event.source.room_id,
+        )
+
+    message = text_generator.generate()
+    line_bot_api.reply_message(event.reply_token, message)
+
+
+@handler.add(PostbackEvent)
+def handle_post_text_message(event):
+    key = "{}_{}".format(event.source.user_id, getattr(event.source, "room_id", ""))
+    data, func_name, woody_type = _handle_postback_data(event.postback)
+    api = JOB_API[woody_type](data=data, key=key)
+    func = "can_{}".format(func_name)
+    try:
+        message = getattr(api, func)()
+
+    except AttributeError:
+        message = TextSendMessage(text="錯誤的訊息")
+
+    finally:
+        line_bot_api.reply_message(event.reply_token, message)
+
+
+def _handle_postback_data(postback):
+    data = {}
+    for pair in parse_qsl(postback.data):
+        data[pair[0]] = pair[1]
+
+    if getattr(postback, "params"):
+        data["target_datetime"] = postback.params["datetime"]
+    return data, data.pop("action"), data.pop("type")
